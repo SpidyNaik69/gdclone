1038fc7aea4b889c09c8a0fa6b8d0f1b958c87e3
SpidyNaik69 <91309844+SpidyNaik69@users.noreply.github.com>
2022-08-24 21:03:19 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -109,9 +109,9 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     buttons.buildbutton(
-        "Repo", "https://www.github.com/yashoswalyo/clone-bot-public"
+        "Owner", "https://t.me/Mr_Spidy"
     )
-    buttons.buildbutton("Report Group", "https://t.me/yash_codes_support")
+    buttons.buildbutton("Channel", "https://t.me/YourDemandZone")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f"""
